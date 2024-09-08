b291632b8905f2611f223941c33461989333a1fb
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-09-08 21:08:15 +0500

    Initial Commit

---
+++ ColorMix/ColorMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixApp.swift
+//  ColorMix
+//
+//  Created by Даниил Сивожелезов on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
