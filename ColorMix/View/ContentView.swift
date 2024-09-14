9742864248380711c6d6c1340723a65a3df7f043
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-09-14 10:26:56 +0500

    no message

--- ColorMix/View/ContentView.swift
+++ ColorMix/View/ContentView.swift
@@ -100,3 +100,19 @@ struct ContentView: View {
         return "#\(redHexValue)\(greenHexValue)\(blueHexValue)"
     }
 }
+
+//extension Color {
+//    func mix(with color: Color) -> Color {
+//        let clampedPercentage = 0.5
+//
+//        let components1 = UIColor(self).cgColor.components!
+//        let components2 = UIColor(color).cgColor.components!
+//
+//        let red = (1.0 - clampedPercentage) * components1[0] + clampedPercentage * components2[0]
+//        let green = (1.0 - clampedPercentage) * components1[1] + clampedPercentage * components2[1]
+//        let blue = (1.0 - clampedPercentage) * components1[2] + clampedPercentage * components2[2]
+//        let alpha = (1.0 - clampedPercentage) * components1[3] + clampedPercentage * components2[3]
+//
+//        return Color(red: red, green: green, blue: blue, opacity: alpha)
+//    }
+//}
